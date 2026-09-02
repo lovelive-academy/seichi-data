54df79cbad2b11fb52d0212cdc6bf6d801127376
marukun712 <marukun530@gmail.com>
2026-09-02 13:03:08 +0900

    Cardの画像についているスタイルを修正

--- ui/components/Card.tsx
+++ ui/components/Card.tsx
@@ -3,38 +3,37 @@ import { Show } from "solid-js";
 import type { FeatureView } from "../../src/schema.ts";
 
 interface Props {
-	feature: FeatureView;
-	onClose: () => void;
+  feature: FeatureView;
+  onClose: () => void;
 }
 
 const Card = (props: Props) => {
-	return (
-		<article
-			style={{
-				position: "fixed",
-				top: "16px",
-				right: "16px",
-				width: "32vw",
-				"z-index": 1,
-			}}
-		>
-			<button type="button" onClick={props.onClose} aria-label="閉じる">
-				<X />
-			</button>
-			<h4>{props.feature.properties.series.name}</h4>
-			<h3>{props.feature.properties.title}</h3>
-			<Show when={props.feature.properties.image}>
-				<img
-					src={props.feature.properties.image}
-					alt={props.feature.properties.title}
-					style={{ width: "100%", height: "180px", "object-fit": "cover" }}
-				/>
-			</Show>
-			<Show when={props.feature.properties.description}>
-				<p>{props.feature.properties.description}</p>
-			</Show>
-		</article>
-	);
+  return (
+    <article
+      style={{
+        position: "fixed",
+        top: "16px",
+        right: "16px",
+        width: "32vw",
+        "z-index": 1,
+      }}
+    >
+      <button type="button" onClick={props.onClose} aria-label="閉じる">
+        <X />
+      </button>
+      <h4>{props.feature.properties.series.name}</h4>
+      <h3>{props.feature.properties.title}</h3>
+      <Show when={props.feature.properties.image}>
+        <img
+          src={props.feature.properties.image}
+          alt={props.feature.properties.title}
+        />
+      </Show>
+      <Show when={props.feature.properties.description}>
+        <p>{props.feature.properties.description}</p>
+      </Show>
+    </article>
+  );
 };
 
 export default Card;
